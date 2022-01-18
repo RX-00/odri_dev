c39b52d1f01d0ccf1593fa461a4291f34073fd7a
xingr-orst <xingr@oregonstate.edu>
2022-01-17 19:13:56 -0500

    finally got everything working, if any future packages mess up, apt remove and then apt install again

---
+++ workspace/install/mim_control/share/mim_control/cmake/mim_controlConfig.cmake
@@ -0,0 +1,38 @@
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was Config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include("${CMAKE_CURRENT_LIST_DIR}/mim_controlTargets.cmake")
+
+include(CMakeFindDependencyMacro)
+
+# we do not add the other dependencies because these are header files lib
+find_dependency(pybind11 CONFIG REQUIRED)
+find_dependency(Eigen3 REQUIRED)
+find_dependency(pinocchio REQUIRED)
+find_dependency(eiquadprog REQUIRED)
+find_dependency(yaml_utils REQUIRED)
+
+check_required_components(mim_control)
